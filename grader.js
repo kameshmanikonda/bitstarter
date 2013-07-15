ed7ee4c785d2e9c6234a99a385621610db8da25a
Ubuntu <ubuntu@ip-172-31-9-62.us-west-2.compute.internal>
2013-07-15 04:09:54 +0000

    grader.js Hw3 changes to read url

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -30,11 +31,13 @@ var CHECKSFILE_DEFAULT = "checks.json";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)){
-	console.log("%s does not exist. Exiting.",instr);
-	process.exit(1); //http://node.js.org/api/process.html#process_process_exit_code
+        console.log("%s does not exist. Exiting.",instr);
+        process.exit(1); //http://node.js.org/api/process.html#process_process_\
+exit_code
   }
     return instr;
 };
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -44,15 +47,41 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
+
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for( var ii in checks){
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
-	}
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+        }
     return out;
+};
+
+var checkHtmlUrl = function(url, checksfile) {
+    console.log("inside chckhtmlfile...");
+    rest.get(url.toString()).on('complete',function(result){
+    //fs.writeFileSync('index3.html',result);
+    //    $ = cheerioHtmlFile("index3.html");
+    if(result instanceof Error){
+	console.log('%s does not exist. Exiting.',url);
+	process.exit(1);
+	 }
+    else {
+	$= cheerio.load(result);
+        var checks = loadChecks(checksfile).sort();
+	var out = {};
+        for( var ii in checks){
+	    var present = $(checks[ii]).length > 0;
+	    out[checks[ii]] = present;
+	    }
+	var outJson = JSON.stringify(out, null, 4);
+	console.log(outJson);
+	return out;
+    }
+	
+    });
 };
 
 var clone = function(fn) {
@@ -62,12 +91,17 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    program.option('-c, --checks <check_file>','Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-.option('-f, --file <html_file>', 'path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT).parse(process.argv);
+    program
+.option('-c, --checks <check_file>','Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+.option('-f, --file <index_file>','Path to index.html', clone(assertFileExists))
+.option('-u, --url <url_name>','Path to index html url')
+.parse(process.argv);
 
-var checkJson = checkHtmlFile(program.file, program.checks);
-var outJson = JSON.stringify(checkJson, null, 4);
-console.log(outJson);
+    if(program.file && program.checks) { var checkJson = checkHtmlFile(program.file,program.checks);
+console.log(JSON.stringify(checkJson,null,4));
+			       }else{
+    var checkJson = checkHtmlUrl(program.url, program.checks);
+					   }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
